b241d0f9bea2733894f9edfa34999c26796b0acb
Alan Dragicevich <aldragicevich@gmail.com>
2024-06-17 23:17:57 +1200

    Randomly spawn asteroids with velocity
    Need to work on spawning them in better random spawn locations.

---
+++ src/asteroid.ts
@@ -0,0 +1,51 @@
+import { Assets, Sprite, type Application } from "pixi.js";
+
+export class Asteroid {
+	private sprite!: Sprite;
+
+	constructor(
+		private velocity: { x: number; y: number },
+		private app: Application,
+	) {}
+
+	getSprite(): Sprite {
+		return this.sprite;
+	}
+
+	async loadAssets() {
+		await Assets.load({
+			src: "./assets/sprites/asteroid.png",
+			alias: "asteroid",
+		});
+		this.sprite = Sprite.from("asteroid");
+		this.sprite.anchor = 0.5;
+		this.sprite.x = Math.round(Math.random()) * 1000 - 1;
+		this.sprite.y = Math.round(Math.random()) * 1000 - 1;
+	}
+
+	update(deltaTime: number) {
+		this.sprite.x += this.velocity.x * deltaTime;
+		this.sprite.y += this.velocity.y * deltaTime;
+
+		this.wrapAround();
+	}
+
+	private wrapAround(): void {
+		const halfWidth = this.sprite.width / 2;
+		const halfHeight = this.sprite.height / 2;
+		const screenWidth = this.app.screen.width;
+		const screenHeight = this.app.screen.height;
+
+		if (this.sprite.x > screenWidth + halfWidth) {
+			this.sprite.x = -halfWidth;
+		} else if (this.sprite.x < -halfWidth) {
+			this.sprite.x = screenWidth + halfWidth;
+		}
+
+		if (this.sprite.y > screenHeight + halfHeight) {
+			this.sprite.y = -halfHeight;
+		} else if (this.sprite.y < -halfHeight) {
+			this.sprite.y = screenHeight + halfHeight;
+		}
+	}
+}
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,7 @@
 import { Application } from "pixi.js";
 import { PlayerShip } from "./ship";
 import { KeyState } from "./key-state";
+import { Asteroid } from "./asteroid";
 
 export class Game {
 	private pixiApplication = new Application();
@@ -18,12 +19,28 @@ export class Game {
 			new KeyState().keysHeld,
 			this.pixiApplication,
 		);
+		const asteroids: Asteroid[] = new Array(5);
+		for (let i = 0; i < asteroids.length; i++) {
+			const asteroid = new Asteroid(
+				{
+					x: Math.round(Math.random()) * 4 - 1,
+					y: Math.round(Math.random()) * 4 - 1,
+				},
+				this.pixiApplication,
+			);
+			asteroids[i] = asteroid;
+			await asteroid.loadAssets();
+			this.pixiApplication.stage.addChild(asteroid.getSprite());
+		}
 		await playerShip.loadAssets();
 		this.pixiApplication.stage.addChild(playerShip.getSprite());
 
 		// Render Loop.
 		this.pixiApplication.ticker.add((ticker) => {
 			playerShip.update(ticker.deltaTime);
+			for (const asteroid of asteroids) {
+				asteroid.update(ticker.deltaTime);
+			}
 		});
 	}
 }
